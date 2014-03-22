587a443217ff42eec5ec0ed3501f468e2d30f9bb
vagrant <vagrant@rails-dev-box>
2014-03-22 11:59:21 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WarrantyManager::Application.config.secret_key_base = 'fb076a4059c8dc62d5da78edded2ee1d79134e3d52d99889ddde4988f67a11dfaad2bc97d5aca41a73ca4eec474846ee7398628b806bba0ea75563f5a86e25b7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WarrantyManager::Application.config.session_store :cookie_store, key: '_warranty_manager_session'
